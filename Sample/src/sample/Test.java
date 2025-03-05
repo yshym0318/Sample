09ea397254b0e2e09ad8663b556e21282d5d4bdc
01061833 <01061833@10.17.53.129>
2025-03-05 19:00:05 +0900

    Gitの練習です

--- Sample/src/sample/Test.java
+++ Sample/src/sample/Test.java
@@ -2,14 +2,11 @@ package sample;
 
 public class Test {
 
-    //メインメソッドです!
     public static void main(String[] args) {
-        //System.out.println("Kiramex");
-        /*
-            System.out.println("Java");
-            System.out.println("学習を始めましょう!");
-        */
-        System.out.println("こっちは表示されるよ!");
-
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+        System.out.println("Gitの練習です");
     }
+
 }
